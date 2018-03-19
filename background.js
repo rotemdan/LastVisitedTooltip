443c77d5e0e80540772f32761beccc0aa6cf9d12
rotemdan <rotemdan@gmail.com>
2018-03-19 07:46:00 +0200

    Updated icon

--- background.js
+++ background.js
@@ -1,7 +1,7 @@
 async function onMessage(request, sender, sendResponse) {
 	switch (request.operation) {
 		case "getVisits":
-			return await browser.history.getVisits({url: request.url});
+			return await browser.history.getVisits({ url: request.url });
 			break;
 	}
 }
